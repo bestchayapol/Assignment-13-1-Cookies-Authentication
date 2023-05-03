00b21442eb5891f7a142b2e71f3c0589c167e2b6
bestchayapol <best37@hotmail.com>
2023-05-03 17:00:38 +0700

    Second Commit - Done both login and register

--- index.js
+++ index.js
@@ -35,50 +35,85 @@ app.post("/login", (req, res) => {
   const { username, password } = req.body;
 
   // Retrieve the hashed password for the given username from the database
-  const sql = mysql.format(
-    "SELECT hashed_password FROM users WHERE username = ?",
-    [username]
-  );
-  connection.query(sql, (err, rows) => {
+  const sql = mysql.format("SELECT * FROM users WHERE username = ?", [
+    username,
+  ]);
+  connection.query(sql, [username, password], (err, rows) => {
     if (err) {
       return res.json({
         success: false,
-        data: null,
-        error: err.message,
       });
     }
 
     numRows = rows.length;
-    if (numRows > 0) {
+    if (numRows != 0) {
       //Verify the password
       const hashedPassword = rows[0].hashed_password;
       bcrypt.compare(password, hashedPassword, (err, result) => {
-        if (err) {
-          res.json({
-            success: false,
-            message: "Internal server error",
-          });
-        }
         if (result) {
           // If the password is correct, respond with a success message
-          res.json({
+          return res.json({
             success: true,
             message: "Login Successful",
           });
         } else {
           // If the password is incorrect, respond with a error message
-          res.json({
+          return res.json({
             success: false,
-            message: "Invalid username or password",
+            message: "Invalid Password",
           });
         }
       });
     } else {
       // If the username does not exist in the database, respond with an error message
-      res.json({
+      return res.json({
         success: false,
-        message: "Invalid username or password",
+        message: "User does not exist in the database",
       });
     }
   });
+});
+
+app.post("/register", (req, res) => {
+  const { username, password } = req.body;
+  let pattern = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)[A-Za-z\d]{8,}$/i;
+  if (!pattern.test(password)) {
+    return res.json({
+      success: false,
+      message:
+        "Password length equals or more than 8 characters. The password contains at least one uppercase, lowercase, and number in the string.",
+    });
+  } else {
+    var today = new Date();
+    var date =
+      today.getFullYear() +
+      "-" +
+      (today.getMonth() + 1) +
+      "-" +
+      today.getDate();
+    var time =
+      today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    created_at = date + " " + time;
+    updated_at = created_at;
+    saltRounds = 10;
+    hash = bcrypt.hashSync(password, saltRounds);
+    var sql = `INSERT INTO users (username, password, hashed_password, created_at, updated_at) VALUES(?, ?, ?, ?, ?)`;
+    connection.query(
+      sql,
+      [username, password, hash, created_at, updated_at],
+      (err, rows) => {
+        if (err) {
+          return res.json({
+            success: false,
+            error: err,
+            message: "Error occur while register user.",
+          });
+        }
+        return res.json({
+          success: true,
+          message: "User register successful.",
+        });
+      }
+    );
+  }
 });
